aa4ea5851c8be4a33ef2de17a382169a777d459f
Amanda Charles <ancharles11@gmail.com>
2016-11-11 14:40:37 -0800

    individula boxes change to red color

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 (function() {
   'use strict';
 
+const gridDiv = document.getElementById('grid');
 
 
+gridDiv.addEventListener('click', (event) => {
+let theBox = event.target;
+console.log(theBox);
+  if (event.target === gridDiv) {
+    return;
+  }
+   theBox.classList.toggle('red');
+} )
 
 
 
