3e6aea9128ed7a1dc2f00315f396c23f4a93b276
Amanda Charles <ancharles11@gmail.com>
2016-11-11 15:01:33 -0800

    added event listeners and pallette boxes

--- app.js
+++ app.js
@@ -2,11 +2,21 @@
   'use strict';
 
 const gridDiv = document.getElementById('grid');
+const pinkColor = document.getElementById('pink');
+const yellowColor = document.getElementById('yellow')
+
+yellowColor.addEventListener('click', (event) => {
+
+})
+
+pinkColor.addEventListener('click', (event) => {
+
+
+})
 
 
 gridDiv.addEventListener('click', (event) => {
 let theBox = event.target;
-console.log(theBox);
   if (event.target === gridDiv) {
     return;
   }
@@ -15,7 +25,7 @@ console.log(theBox);
 
 
 
-
+if pallette box on, toggle theBox
 
 
 
