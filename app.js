2c94268eb6e879df8800ffebced72b9597b21edc
Amanda Charles <ancharles11@gmail.com>
2016-11-11 18:21:52 -0800

    works for 4 squares!!

--- app.js
+++ app.js
@@ -5,55 +5,32 @@ const gridDiv = document.getElementById('grid');
 const paletteDiv = document.getElementById('thePalette')
 const colorsInPalette = document.getElementById('thePalette').children;
 let theOnColor;
+let useColor
 
 paletteDiv.addEventListener('click', (event) => {
   theOnColor = event.target;
+  useColor = theOnColor.getAttribute('id');
   if (event.target === paletteDiv){
     return;
   }
-
-
   for (let i = 0; i < colorsInPalette.length; i++) {
     colorsInPalette[i].classList.remove('on');
   }
-
   theOnColor.classList.toggle('on');
-  console.log(theOnColor);
 
 })
-// ****************************
 
 
-
-
-
-
-
-// *******************
 
 gridDiv.addEventListener('click', (event) => {
-  let useColor = theOnColor.getAttribute('id');
-
-console.log(useColor);
 let theBox = event.target;
   if (event.target === gridDiv) {
     return;
   }
-   theBox.classList.toggle(useColor);
 
 
+  theBox.style.backgroundColor = useColor;
+console.log(theBox.classList);
+console.log(useColor);
 } )
-
-
-
-
-
-
-
-
-
-
-
-
-
 })();
